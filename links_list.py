ffd113c0f59c7cd4cb55f5067ca5767459586d5f
adnankattekaden <adnankattekaden2020@gmail.com>
2021-07-05 14:24:26 +0530

    fetcher inital commit

---
+++ links_list.py
@@ -0,0 +1,23 @@
+urls = [
+    {'carrot':'https://www.luluhypermarket.in/en-in/fresh-carrot-approx-500g/p/1113343'},
+    {'tomato':'https://www.luluhypermarket.in/en-in/tomato-approx-550g-to-600g/p/7649_EA'},
+    {'onion':'https://www.luluhypermarket.in/en-in/fresh-onion-red-approx-1kg/p/1113342'},
+
+    {'carrot':'https://www.klfresh.com/products/product-detail/carrots/1108'},
+    {'tomato':'https://www.klfresh.com/products/product-detail/tomato/1154'},
+    {'onion':'https://www.klfresh.com/products/product-detail/onionulli/1135'},
+
+    {'carrot':'https://www.kada.in/product_detail.php?pdt=carrot-500g'},
+    {'tomato':'https://www.kada.in/product_detail.php?pdt=tomato-500g'},
+    {'onion':'https://www.kada.in/product_detail.php?pdt=big-onion-savala-1kg'},
+
+    {'carrot':'https://www.jiomart.com/p/groceries/carrot-regular-1-kg/590003546'},
+    {'tomato':'https://www.jiomart.com/p/groceries/amaranthus-1-bunch/590003517'},
+    {'onion':'https://www.jiomart.com/p/groceries/lentil-sprouts-200-g/590003515'},
+
+    {'carrot':'https://www.bigbasket.com/pd/10000271/fresho-carrot-ooty-500-g/?nc=feat-prod&t_pg=l1-cat-fruits-vegetables&t_p=cl-temp-1&t_s=feat-prod&t_pos_sec=3&t_pos_item=1&t_ch=desktop'},
+    {'tomato':'https://www.bigbasket.com/pd/10000203/fresho-tomato-local-1-kg/?nc=cl-prod-list&t_pg=&t_p=&t_s=cl-prod-list&t_pos=1&t_ch=desktop'},
+    {'onion':'https://www.bigbasket.com/pd/10000148/fresho-onion-medium-vengayam-1-kg/?nc=cl-prod-list&t_pg=&t_p=&t_s=cl-prod-list&t_pos=1&t_ch=desktop'},
+
+]
+
